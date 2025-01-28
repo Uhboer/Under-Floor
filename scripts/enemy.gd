04bfe433e7942f850174ea46f48fc35c8fd8f5e7
NeverMind92 <llegionov2@gmail.com>
2025-01-28 20:43:00 +0400

    refactor kill_player + new tweak: retreat

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,12 +4,16 @@ extends CharacterBody3D
 @onready var character = $"../character"
 
 const MOVE_SPEED = 2.0
+const RETREAT = 2.0
 const ATTACK_RANGE = 2.0
 const DAMAGE = 30.0
 
 var hp = 100.0
 
+var can_attack = true
 var is_dead = false
+var is_retreat = false
+var retreat_target_position: Vector3
 
 func _physics_process(delta: float) -> void:
 	if is_dead:
@@ -19,31 +23,39 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	var dir = character.global_position - global_position
-	dir.y = 0.0
-	dir = dir.normalized()
+
 	
-	velocity = dir * MOVE_SPEED
-	
-	move_and_slide()
-	kill_player()
+	if is_retreat: 
+		global_position = global_position.lerp(retreat_target_position, MOVE_SPEED * 0.04)
+		if global_position.distance_to(retreat_target_position) < 0.1:
+			is_retreat = false
+	else:
+		var dir = character.global_position - global_position
+		dir.y = 0.0
+		dir = dir.normalized()
+		velocity = dir * MOVE_SPEED
+		move_and_slide()
+		kill_player()
 	
 func kill_player():
-	var can_attack = false
 	var eye_line = Vector3.UP * 1.5
 	var query = PhysicsRayQueryParameters3D.create(global_position+eye_line, character.global_position+eye_line, 1)
 	var result = get_world_3d().direct_space_state.intersect_ray(query)
 	var dist_to_player = global_position.distance_to(character.global_position)
-	if dist_to_player > ATTACK_RANGE:
-		can_attack = false
-		return
-	elif dist_to_player <= ATTACK_RANGE:
+	
+	if dist_to_player <= ATTACK_RANGE and result.is_empty():
+		if can_attack:
+			character.take_damage(DAMAGE)
+			retreat()
+			can_attack = false
+	else:
 		can_attack = true
-		if result.is_empty():
-			if can_attack:
-				character.take_damage(DAMAGE)
-				can_attack = false
 
+func retreat():
+	var retreat_dir = (global_position - character.global_position).normalized()
+	retreat_target_position = global_position + retreat_dir * RETREAT
+	is_retreat = true
+	
 #TODO: uncomment this shit after when making sprites
 #func get_killed():
 	#if is_dead:
